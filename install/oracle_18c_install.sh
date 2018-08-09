7a9580cb36bfcbad1c665e899e37603fc63c16b6
dockerhelp <42245293+dockerhelp@users.noreply.github.com>
2018-08-10 02:30:15 +0530

    Update oracle_18c_install.sh

--- install/oracle_18c_install.sh
+++ install/oracle_18c_install.sh
@@ -1,8 +1,17 @@
 #!/bin/bash
 
 set -e
+export INSTALL=$HOME/docker-oracle-ee-base-18c/install
 
-RSP = $HOME/docker-oracle-ee-base-18c/install/oracle-18c-ee.rsp
+echo "Creating Directory"
+groupadd dba && useradd -m -G dba oracle
+rm -rf /u01
+mkdir /u01 && mkdir -p /u01/app/oracle/product/18.0.0/dbhome_1 && chown -R oracle:dba /u01 && chmod -R 775 /u01
+
+echo "Setting ENV"
+echo oracle:oracle | chpasswd
+export ORACLE_BASE=/u01/app/oracle
+export ORACLE_HOME=/u01/app/oracle/product/18.0.0/dbhome_1
 
 groupadd dba && useradd -m -G dba oracle
 rm -rf /u01
@@ -16,24 +25,38 @@ echo "Downloading oracle database zip"
 wget -q --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id=15CLHkPZzwih26oINeXvIB79Jny8zgqWh' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=15CLHkPZzwih26oINeXvIB79Jny8zgqWh" -O oracle_database.zip && rm -rf /tmp/cookies.txt
 
 echo "Extracting oracle database zip"
-su oracle -c 'unzip -q /oracle_database.zip -d /u01/app/oracle/product/18.0.0/dbhome_1/'
-rm -f /oracle_database.zip
-cp $HOME/docker-oracle-ee-base-18c/install/oracle-18c-ee.rsp $ORACLE_HOME/oracle-18c-ee.rsp
-cp $HOME/docker-oracle-ee-base-18c/install/oracle-18c-ee.rsp $ORACLE_HOME/dbca_18c.rsp
+su oracle -c 'unzip -q oracle_database.zip -d /u01/app/oracle/product/18.0.0/dbhome_1/'
+#rm -f /oracle_database.zip
+
+echo "setting up Response files"
+cp $INSTALL/oracle-18c-ee.rsp $ORACLE_HOME/oracle-18c-ee.rsp
+cp $INSTALL/oracle-18c-ee.rsp $ORACLE_HOME/dbca_18c.rsp
 chmod 777 $ORACLE_HOME/oracle-18c-ee.rsp
 chmod 777 $ORACLE_HOME/dbca_18c.rsp
 
-#Run installer
-#su oracle -c "cd $ORACLE_HOME && ./runInstaller -skipPrereqs -silent -responseFile $RSP -waitForCompletion"
-#Run Root.sh
-/u01/app/oraInventory/orainstRoot.sh > /dev/null 2>&1
-echo | /u01/app/oracle/product/18.0.0/dbhome_1/root.sh > /dev/null 2>&1 || true
+su oracle -c "$ORACLE_HOME/runInstaller -force -skipPrereqs -silent -responseFile $ORACLE_HOME/oracle-18c-ee.rsp -waitForCompletion"
+
+echo "Done"
+
+#Cleanup
+echo "Cleaning up"
+rm -rf /home/oracle/database /tmp/*
+
+#Connect to Oracle
+su - oracle <<EOF
+id
+export ORACLE_BASE=/u01/app/oracle
+export ORACLE_HOME=/u01/app/oracle/product/18.0.0/dbhome_1
+export PATH=$ORACLE_HOME/bin:$PATH
+sqlplus -v
+EOF
+
 #database installation
 echo "Default 18c database install with PDB"
 dbca -silent -createDatabase -responseFile $ORACLE_HOME/dbca_18c.rsp
+
 #Cleanup
 echo "Cleaning up"
-#rm -rf /home/oracle/database /tmp/*
+rm -rf /home/oracle/database /tmp/*
 
-#Move product to custom location
-#mv /u01/app/oracle/product /u01/app/oracle-product
+echo "DataBase Installed!!!"
